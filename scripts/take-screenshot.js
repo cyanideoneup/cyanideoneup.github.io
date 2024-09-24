a3b49194decad4d53516b73d5c5d6524fd07f403
cyanideoneup <kevingn0301@gmail.com>
2024-09-24 19:59:34 -0400

    Update take-screenshot.js

--- scripts/take-screenshot.js
+++ scripts/take-screenshot.js
@@ -3,13 +3,17 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  await page.setViewport({ width: 640, height: 360 });
 
   await page.goto("https://cyanone.art/portfolio", {
     waitUntil: "networkidle2",
   });
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await page.screenshot({ path: "screenshot.png", fullPage: true });
+  await page.screenshot({
+    path: "screenshot.png",
+    clip: { x: 0, y: 0, width: 640, height: 360 },
+  });
 
   await browser.close();
 })();
